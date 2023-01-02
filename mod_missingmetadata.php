7d660f7b65aebf4563137e0fbcb1986a659cf698
sergiois <ser.iglesias@gmail.com>
2023-01-02 18:20:58 +0100

    Version 1.0.2: update to PHP 8

--- mod_missingmetadata.php
+++ mod_missingmetadata.php
@@ -14,12 +14,13 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 
 // Include dependancies.
 require_once dirname(__FILE__).'/helper.php';
+$version = new Joomla\CMS\Version();
 
 $list = modMissingMetaDataHelper::getList($params);
-if(Version::isCompatible("4.0")){
+if($version->isCompatible("4.0")){
 	$params->set('layout','default4');
 }
-elseif (Version::isCompatible("3.0")){
+elseif ($version->isCompatible("3.0")){
 	$params->set('layout','default3');
 } else {
 	$params->set('layout','default');
